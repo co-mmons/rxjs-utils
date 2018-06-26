58a87ee5be1ea3117a347b5f16c8340282f753ec
Levy <levy@codeandmore.pl>
2018-06-26 15:03:54 +0200

    Unsubscribe fix

--- src/unsubscribe.ts
+++ src/unsubscribe.ts
@@ -1,6 +1,6 @@
 import {Subscription} from "rxjs/Subscription";
 
-export function unsubscribe(subscription: Function | Subscription) {
+export function unsubscribe(subscription: Function | Subscription | {unsubscribe: () => any}) {
 
     if (subscription) {
         if (typeof subscription == "function") {
